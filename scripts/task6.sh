914d14f56ff656615ed01808d5ea36d40b76a94c
Vikrant Pandey <vikrantpandey466@gmail.com>
2023-06-26 15:32:09 +0530

    Task 6 complete

--- scripts/task6.sh
+++ scripts/task6.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+download_files() {
+    local remoteHost=$1
+    local remotePath=$2
+    local localPath=$3
 
-# download_files() {
-#     local remote_host=$1
-#     local remote_path=$2
-#     local local_path=$3
+    ssh "$remoteHost" "cd \"$remotePath\" && tar cf - ./" | tar xf - -C "$localPath"
+    echo "Files downloaded successfully"
+}
 
-#     ssh "$remote_host" "cd $remote_path && find . -type f -exec sh -c 'mkdir -p \"$local_path/$(dirname {})\" && cp -p {} \"$local_path/{}\"' \;"
-# }
+# read input
+read -r -p "Enter remote host: " remoteHost
+read -r -p "Enter remote path: " remotePath
+read -r -p "Enter local path: " localPath
 
-# read -p "Enter remote host: " remote_host
-# read -p "Enter remote path: " remote_path
-# read -p "Enter local path: " local_path
+remoteHost="vikrant@192.168.43.23"
+remotePath="/home/vikrant/Music"
+localPath="D:\IESoft"
 
-# download_files "$remote_host" "$remote_path" "$local_path"
+download_files "$remoteHost" "$remotePath" "$localPath"
